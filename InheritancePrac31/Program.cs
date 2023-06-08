5f6487c2e8f4e34dd88dfdbc64a9e306e2c1a6de
Gihun Ham <ham@e-mirim.hs.kr>
2023-06-08 14:07:27 +0900

    초기 커밋.

---
+++ InheritancePrac31/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePrac31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+        }
+    }
+}
